500bc1a645e00bcc0eaf65b655e2b2bbebf0ca00
Jaque Laurenti <jaquelinelaurenti@gmail.com>
2023-08-09 21:59:05 -0300

    aulas

---
+++ Aula 04/buscaSimples/soma.js
@@ -0,0 +1,3 @@
+export function soma (valora, valorb) {
+  return valora + valorb;
+}
---
+++ Aula 04/index.js
@@ -0,0 +1,4 @@
+import { buscaSimples } from "./buscaSimples/busca";
+import { soma } from "./buscaSimples/soma"
+
+console.log(soma(1,2));
