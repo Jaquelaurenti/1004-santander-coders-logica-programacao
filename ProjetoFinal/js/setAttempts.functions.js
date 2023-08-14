481916d113c0f2d117f56fa7511cf80032ed0ade
GleidsonLM <gleidsonlm@gmail.com>
2023-08-14 20:42:55 -0300

    add: handleLetterNotFound() un-hidden bodyParts on attempt fail.

--- ProjetoFinal/js/setAttempts.functions.js
+++ ProjetoFinal/js/setAttempts.functions.js
@@ -5,6 +5,7 @@ export function setAttempts() {
     document.querySelectorAll('.letter-button').forEach(button => {
         button.addEventListener('click', () => {
             button.disabled = true;
+            letterFound = false;
 
             handleLetter(button);
         });
@@ -15,70 +16,70 @@ export function setAttempts() {
 
 function handleLetter(button) { 
     document.querySelectorAll('.letter').forEach(letter => {
-    /*         
-        if (letter.textContent === button.id) {
-            letterFound = true;
-            letter.classList.remove('hidden');
+        switch (letter.textContent) {
+            case 'a':
+            case 'á':
+            case 'ã':
+            case 'â':
+                if (button.id === 'a') {
+                    handleLetterFound(letter)
+                }
+                break;
+            case 'e':
+            case 'é':
+            case 'ê':
+                if (button.id === 'e') {
+                    handleLetterFound(letter)
+                }
+                break;
+            case 'i':
+            case 'í':
+                if (button.id === 'i') {
+                    handleLetterFound(letter)
+                }
+                break;
+            case 'o':
+            case 'ó':
+            case 'ô':
+            case 'õ':
+                if (button.id === 'o') {
+                    handleLetterFound(letter)
+                }
+                break;
+            case 'u':
+            case 'ú':
+            case 'ü':
+                if (button.id === 'u') {
+                    handleLetterFound(letter)
+                }
+                break;
+            default:
+                if (letter.textContent === button.id) {
+                    handleLetterFound(letter)
+                }
+                break;
         }
-    */
-
-   switch (letter.textContent) {
-        case 'a':
-        case 'á':
-        case 'ã':
-        case 'â':
-            if (button.id === 'a') {
-                handleLetterFound(letter)
-            }
-            break;
-        case 'e':
-        case 'é':
-        case 'ê':
-            if (button.id === 'e') {
-                handleLetterFound(letter)
-            }
-            break;
-        case 'i':
-        case 'í':
-            if (button.id === 'i') {
-                handleLetterFound(letter)
-            }
-            break;
-        case 'o':
-        case 'ó':
-        case 'ô':
-        case 'õ':
-            if (button.id === 'o') {
-                handleLetterFound(letter)
-            }
-            break;
-        case 'u':
-        case 'ú':
-        case 'ü':
-            if (button.id === 'u') {
-                handleLetterFound(letter)
-            }
-            break;
-        default:
-            if (letter.textContent === button.id) {
-                handleLetterFound(letter)
-            }
-            break;
-    }   
-    })
+    });
 
     if (!letterFound) {
-        attemptsCount--;
-
-        document.getElementById('attempts').textContent = attemptsCount;
+        handleLetterNotFound()
     }
 
     handleGameEnd(attemptsCount)
 }
 
+function handleLetterNotFound() {
+    attemptsCount--;
+    document.getElementById('attempts').textContent = attemptsCount;
+
+    const bodyParts = document.querySelectorAll('.forca-parte');
+    bodyParts[attemptsCount].classList.remove('hidden')
+}
+
 function handleLetterFound(letter) {
     letterFound = true;
     letter.classList.remove('hidden');
+
 }
 
 function handleGameEnd(attempts) {
--- ProjetoFinal/js/setBodyParts.function.js
+++
@@ -1,5 +0,0 @@
-const bodyParts = document.querySelectorAll('.forca-parte');
-
-export default function setBodyParts() {
-    bodyParts.length
-}
